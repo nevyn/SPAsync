d00410d7610709248ccc23729643938aa1412c9b
Joachim Bengtsson <joachimb@gmail.com>
2013-01-30 12:42:58 +0100

    Umbrella header

--- include/SPAsync/SPAsync.h
+++ include/SPAsync/SPAsync.h
@@ -1,13 +1,2 @@
-//
-//  SPAsync.h
-//  SPAsync
-//
-//  Created by Joachim Bengtsson on 2013-01-29.
-//  Copyright (c) 2013 Spotify. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@interface SPAsync : NSObject
-
-@end
+#import <SPAsync/SPTask.h>
+#import <SPAsync/SPAgent.h>
