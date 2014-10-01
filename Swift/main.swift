b623b8cb5d25a9846132d41635e0331ca8a23c18
Joachim Bengtsson <joachimb@gmail.com>
2014-10-01 14:58:48 +0200

    Make swift demo use the Swift-only Task

--- Swift/main.swift
+++ Swift/main.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-func slowAddition(a: Int, b: Int, callback: (sum: NSNumber, error: NSError?) -> Void) {
+func slowAddition(a: Int, b: Int, callback: (sum: Int, error: NSError?) -> Void) {
 	let sum = a + b
-	callback(sum: NSNumber(integer: sum), error: nil)
+	callback(sum: sum, error: nil)
 }
 
-func taskize<P1, P2, R1 : AnyObject> (
+func taskize<P1, P2, R1> (
 	asyncFunc: (
 		p1: P1,
 		p2: P2,
@@ -14,37 +14,37 @@ func taskize<P1, P2, R1 : AnyObject> (
 			err: NSError?
 		) -> Void
 	) -> Void
-) -> (P1, P2) -> SPTask
+) -> (P1, P2) -> Task<R1>
 {
-	let source = SPTaskCompletionSource()
+	let source = TaskCompletionSource<R1>()
 	
-	return { (p1: P1, p2: P2) -> SPTask in
+	return { (p1: P1, p2: P2) -> Task<R1> in
 		asyncFunc(p1: p1, p2: p2, { (r1: R1, error: NSError?) -> Void in
 			source.completeWithValue(r1)
 		})
-		return source.task()
+		return source.task
 	}
 }
 
 class Hello {
-	func slowAddition(a: Int, b: Int, callback: (sum: NSNumber, error: NSError?) -> Void) {
+	func slowAddition(a: Int, b: Int, callback: (sum: Int, error: NSError?) -> Void) {
 		let sum = a + b
-		callback(sum: NSNumber(integer: sum), error: nil)
+		callback(sum: sum, error: nil)
 	}
 }
 
 // Calling a function with three parameters: two ints and a callback.
-slowAddition(4, 5) { (sum: NSNumber, error: NSError?) -> Void in
+slowAddition(4, 5) { (sum: Int, error: NSError?) -> Void in
 	println("Look ma, callback summarized! \(sum)")
 }
 
 // Creating and calling a function with two parameters (two ints) that returns a task!
-taskize(slowAddition)(4, 5).addCallback({ (sum: AnyObject!) -> () in
+taskize(slowAddition)(4, 5).addCallback({ (sum: Int) -> () in
 	println("Look ma, future summarized! \(sum)")
 })
 
 let hello = Hello()
-taskize(hello.slowAddition)(4, 6).addCallback({ (sum: AnyObject!) -> () in
+taskize(hello.slowAddition)(4, 6).addCallback({ (sum: Int) -> () in
 	println("From an object even! \(sum)")
 })
 
